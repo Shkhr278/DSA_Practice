777b17ae4d9b312d52602ee320b592d13ba85936
Shkhr278 <shikhars.singh27@gmail.com>
2024-09-18 18:35:44 +0530

    n changed

--- DSA/integerfreq.java
+++ DSA/integerfreq.java
@@ -10,7 +10,7 @@ public class integerfreq {
     public static void main(String[] args) {
 
         //Taking number as a string input
-        String n = "12228645867";
+        String n = "1245228645867";
 
     //Calling find function
      find(n);
