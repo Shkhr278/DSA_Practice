902d7db3b966a8a1d5632e7cee6bd4dfc2afa8eb
Shkhr278 <shikhars.singh27@gmail.com>
2024-09-18 18:20:05 +0530

    First commit

---
+++ DSA/integerfreq.java
@@ -0,0 +1,52 @@
+/*
+ Question: 
+ */
+
+package DSA;
+
+public class integerfreq {
+
+    //Main Function
+    public static void main(String[] args) {
+
+        //Taking number as a string input
+        String n = "abdueavfdbuyegvduya";
+
+    //Calling find function
+     find(n);
+
+    //  0 -> 48
+    //  1 -> 49
+    //  .
+    //  .
+    //  9 -> 57
+    }
+
+    //Function to find frequency
+    public static void find(String n) {
+
+        //Array to store frequency of each digit
+        int[] freq = new int[10];
+
+        //Index to iterate over the String
+        int l=0;
+
+        //Iterating over the String
+        while (l<n.length()) {
+
+            //Extracting ASCII value of each number character
+            int r=(int)n.charAt(l);
+
+            //Incrementing the frequency of that number
+            freq[r-48]++;       
+
+            //Updating the index
+            l+=1;           
+        }
+
+        //
+        for(int i=0; i<10; i+=1)
+            if(freq[i]>0) System.out.println("Frequency of "+ i + " is:"+freq[i]);
+
+    }
+}
