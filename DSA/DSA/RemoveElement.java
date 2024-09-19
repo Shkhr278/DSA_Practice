7dad43be3d9c1711ab19de83be3ea508ade915c2
Shkhr278 <shikhars.singh27@gmail.com>
2024-09-19 23:24:26 +0530

    new code added

--- DSA/DSA/RemoveElement.java
+++ DSA/DSA/RemoveElement.java
@@ -1,16 +1,31 @@
 package DSA;
 
 public class RemoveElement {
-        public static int removeElement(int[] nums, int val) {
-            int k = 0; // This will be the index to place the non-val elements
-            
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] != val) {
-                    nums[k] = nums[i]; // Place the current element in the correct position
-                    k++; // Move to the next position for the next non-val element
-                }
+    public int removeElement(int[] nums, int val) {
+        int k = 0; // Keeps track of the count of non-val elements
+        // Iterate through the array
+        for (int i = 0; i < nums.length; i++) {
+            // If the element is not equal to val, move it to the front
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
             }
-            
-            return k; // k is the length of the array with all val elements removed
+        }
+        return k; // Return the count of non-val elements
+    }
+
+    public static void main(String[] args) {
+        RemoveElement solution = new RemoveElement();
+        int[] nums = {3, 2, 2, 3}; // Example input
+        int val = 3;
+        
+        int k = solution.removeElement(nums, val); // Call the method
+        
+        // Output the result
+        System.out.println("Number of elements not equal to " + val + ": " + k);
+        System.out.print("Modified array: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
         }
     }
+}
