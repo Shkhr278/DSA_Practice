9de5b483bb697cd6528ab33f006237468beb86c0
Shkhr278 <shikhars.singh27@gmail.com>
2024-09-25 23:01:55 +0530

    new code

--- Java Training Questions/Factorial.java
+++ Java Training Questions/Factorial.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class Factorial {
-    
+    public static int findFactorial(int number) {
+        int result = 1;
+
+        for (int i = 2; i <= number; i++) {
+            result *= i; 
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter a number to find its factorial: ");
+        int num = input.nextInt(); 
+
+        int factorial = findFactorial(num);
+
+        System.out.println("The factorial of " + num + " is: " + factorial);
+
+        input.close();
+    }
 }
